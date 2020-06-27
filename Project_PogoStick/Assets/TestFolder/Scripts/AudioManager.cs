5e8aec0f3310e99a97697a493cc84292ecd05814
judas13 <kynaore13@gmail.com>
2020-06-27 19:33:40 +0900

    テストシーン作成。AudioManagerの作成。

---
+++ Project_PogoStick/Assets/TestFolder/Scripts/AudioManager.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// オーディオ管理クラス
+/// </summary>
+public class AudioManager : SingletonMonoBehaviour<AudioManager> {
+
+	/// <summary>
+	/// BGM定義
+	/// </summary>
+	public enum BGM_TYPE {
+		FIRST
+	}
+
+	/// <summary>
+	/// SE定義
+	/// </summary>
+	public enum SE_TYPE {
+		FIRST
+	}
+
+	/// <summary>
+	/// BGM用AudioSource
+	/// </summary>
+	private AudioSource bgmSource;
+
+	/// <summary>
+	/// SE用AudioSource
+	/// </summary>
+	private AudioSource seSource;
+
+	/// <summary>
+	/// 再生BGMファイル
+	/// </summary>
+	[SerializeField, Header("再生BGMファイル")]
+	private AudioClip[] bgmClip;
+
+	/// <summary>
+	/// 再生SEファイル
+	/// </summary>
+	[SerializeField, Header("再生SEファイル")]
+	private AudioClip[] seClip;
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	private void Awake() {
+
+		base.Awake();
+
+		//シーン遷移で消えないように設定。
+		DontDestroyOnLoad(this);
+
+		//BGMと効果音分のオーディオソースを追加。
+		for (int i = 0; i < 2; i++) this.gameObject.AddComponent<AudioSource>();
+
+		//オーディオソースの割り当ての設定。
+		var audioSources = GetComponents<AudioSource>();
+		bgmSource = audioSources[0];
+		bgmSource.playOnAwake = false;
+		seSource = audioSources[1];
+		seSource.playOnAwake = false;
+	}
+
+
+	//BGMの再生・停止に関して-------------------------------------------------------------------------------------------------------------------
+	#region //BGMの再生・停止に関して-------------------------------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// 指定したBGMを再生する。
+	/// </summary>
+	/// <param name="_bgmType"></param>
+	/// <param name="_vol"></param>
+	/// <param name="_delay"></param>
+	/// <param name="_loop"></param>
+	public void PlayBGM(BGM_TYPE _bgmType, float _vol = 1, float _delay = 0, bool _loop = true) {
+		//BGM再生コルーチンの再生。
+		StartCoroutine(_PlayBGM(_bgmType, _vol, _delay, _loop));
+	}
+
+	/// <summary>
+	/// BGM再生コルーチン
+	/// </summary>
+	/// <param name="_bgmType"></param>
+	/// <param name="_vol"></param>
+	/// <param name="_delay"></param>
+	/// <param name="_loop"></param>
+	/// <returns></returns>
+	private IEnumerator _PlayBGM(BGM_TYPE _bgmType, float _vol, float _delay, bool _loop) {
+
+		//ディレイの設定。
+		yield return new WaitForSeconds(_delay);
+
+		//BGMの設定。
+		bgmSource.clip = bgmClip[(int)_bgmType];
+
+		//ボリュームの設定。
+		bgmSource.volume = _vol;
+
+		//ループの設定。
+		bgmSource.loop = _loop;
+
+		//BGMの再生。
+		bgmSource.Play();
+
+		//コルーチンの終了。
+		yield break;
+	}
+
+	/// <summary>
+	/// BGMの停止
+	/// </summary>
+	public void Stop_BGM() {
+		bgmSource.Stop();
+	}
+
+	#endregion //BGMの再生・停止に関して-------------------------------------------------------------------------------------------------------------------
+
+
+	//SEの再生・停止に関して--------------------------------------------------------------------------------------------------------------------
+	#region //SEの再生・停止に関して--------------------------------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// 指定したSEを再生する。
+	/// </summary>
+	/// <param name="_seType"></param>
+	/// <param name="_vol"></param>
+	/// <param name="_delay"></param>
+	/// <param name="_loop"></param>
+	public void Play_SE(SE_TYPE _seType, float _vol = 1, float _delay = 0, bool _loop = false) {
+		//SE再生コルーチンの再生。
+		StartCoroutine(_PlaySE(_seType, _vol, _delay, _loop));
+	}
+
+	/// <summary>
+	/// SE再生コルーチン
+	/// </summary>
+	/// <param name="_seType"></param>
+	/// <param name="_vol"></param>
+	/// <param name="_delay"></param>
+	/// <param name="_loop"></param>
+	/// <returns></returns>
+	private IEnumerator _PlaySE(SE_TYPE _seType, float _vol, float _delay, bool _loop) {
+
+		//ディレイの設定。
+		yield return new WaitForSeconds(_delay);
+
+		//ループ設定の場合
+		if (_loop)
+		{
+
+			//SEの設定。
+			seSource.clip = seClip[(int)_seType];
+
+			//ボリュームの設定。
+			seSource.volume = _vol;
+
+			//ループの設定。
+			seSource.loop = _loop;
+
+			//BGMの再生。
+			seSource.Play();
+
+			//コルーチンの終了。
+			yield break;
+
+		}
+		else //単発再生の場合 
+		{
+			seSource.PlayOneShot(seClip[(int)_seType], _vol);
+
+			//コルーチンの終了。
+			yield break;
+		}
+	}
+
+	/// <summary>
+	/// SEの停止。
+	/// </summary>
+	public void Stop_SE() {
+		seSource.Stop();
+	}
+
+	#endregion //SEの再生・停止に関して--------------------------------------------------------------------------------------------------------------------
+
+
+}
---
+++ Project_PogoStick/Assets/TestFolder/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// シングルトンクラス
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour {
+
+	/// <summary>
+	/// 可変インスタンス
+	/// </summary>
+	private static T _instance;
+
+	/// <summary>
+	/// インスタンス取得プロパティ
+	/// </summary>
+	public static T Instance {
+		get {
+			if (_instance == null) {
+				_instance = FindObjectOfType<T>();
+				if (_instance == null) {
+					Debug.LogError(typeof(T) + "のインスタンスが見つかりません");
+				}
+			}
+			return _instance;
+		}
+	}
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	protected void Awake() {
+		CheckInstance();
+	}
+
+	/// <summary>
+	/// インスタンスの確認
+	/// </summary>
+	/// <returns></returns>
+	protected bool CheckInstance() {
+		if (this == Instance){
+			return true;
+		} else{
+			Destroy(gameObject);
+			return false;
+		}
+	}
+
+}
